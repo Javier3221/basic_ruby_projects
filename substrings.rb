7009a4e82632014fc6c8f7f82fc71b1c46cb5f3c
Javier Perez <javierpdc13@hotmail.com>
2022-12-31 12:10:51 -0400

    Complete substrings exercise

---
+++ substrings.rb
@@ -0,0 +1,18 @@
+def substrings(string, dictionary)
+  return "No empty strings" if string == ""
+  string_array = string.split("")
+
+  dictionary.reduce(Hash.new(0)) do |count, substr|
+    string_array.each_index do |index|
+      string_fragment = string_array[index..index+(substr.length-1)]
+      if substr == string_fragment.join
+        count[substr.to_sym] += 1
+      end
+    end
+
+    count
+  end
+end
+
+dictionary = ["be", "below", "low"]
+puts substrings("bebelow lowbelow", dictionary)
